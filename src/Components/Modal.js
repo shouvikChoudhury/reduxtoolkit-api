58b22986a1161eab4849f31c062f05635e9b5463
shouvikChoudhury <shouvikpgec50@gmail.com>
2025-03-08 00:06:05 +0530

    Add files via upload

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -6,17 +6,8 @@ function Modal({ setOpenModal }) {
     return (
         <div className="modalBackground" onClick={(event) => { setOpenModal(false); event.stopPropagation() }}>
             <div className="modalContainer" onClick={(event) => { event.stopPropagation() }}>
-                <div className="titleCloseBtn">
-                    <button onClick={() => { setOpenModal(false) }}>X</button>
-                </div>
-                <div className="title">
-                    <h1>Add task to todo !</h1>
-                </div>
                 <div className="body">
                     <TaskForm setOpenModal={setOpenModal} />
-                </div>
-                <div className="footer">
-                    <button onClick={() => { setOpenModal(false) }} id="cancelBtn">Cancel</button>
                 </div>
             </div>
         </div>
